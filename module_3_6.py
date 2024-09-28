46944c7a52bc83cb60a47305018db26a0d933988
PetrovDmitriyV <phanl2@mail.ru>
2024-09-28 15:21:04 +0300

    Update module_3_6.py

--- module_3_6.py
+++ module_3_6.py
@@ -4,61 +4,67 @@ def calculate_structure_sum(data_structure):
     a2 = 0
     a3 = 0
     a4 = 0
+    a5 = 0
+    n = 0
+    m = 0
+    z = 0
     sums = data_structure
     for key in sums:
         # print(key)
         for p in range(len(key)):
-            a = a1 + a2 + a3 + a4
-            if isinstance(key, list) == True:
+            z = a1 + a2 + a3 + a4 + a5      # 6 11 5 29 48
+            if isinstance(key, list) == True:  # [1, 2, 3]
                 for i in key:
-                    a = a + i
-                a1 = a
+                    m = m + i
+                a1 = m      # a1 = 6
                 break
-            if isinstance(key, str) == True:
-                a = a + len(key)
-                a2 = a
-                break
-            if isinstance(key, dict) == True:
+            if isinstance(key, dict) == True:  # {'a': 4, 'b': 5}
                 b = key
                 for key, value in b.items():
-                    a = a + len(key) + value
-                a3 = a
+                    n = n + len(key) + value
+                a2 = n      # 11
                 break
-            if isinstance(key, tuple) == True:
+            if isinstance(key, str) == True:  # Hello
+                a3 = + len(key)     # a3 = 5
+                break
+
+            if isinstance(key, tuple) == True:  # (6, {'cube': 7, 'drum': 8}) ((), [{(2, 'Urban', ('Urban2', 35))}])
                 c = key
                 for key in c:
-                    if isinstance(key, int) == True:
-                        a = key
-                        break
-                    if isinstance(key, dict) == True:
+                    if isinstance(key, int) == True:    # 6
+                        m = key       # a4 = 6
+
+                    if isinstance(key, dict) == True:   # 23 = {'cube': 7, 'drum': 8}
                         d = key
                         for key, value in d.items():
-                            a = a + len(key) + value
+                            m = m + len(key) + value
+                        a4 = m      # a4 = 29 = 6 + 23
                         break
-                    if isinstance(key, list) == True:
+                    if isinstance(key, list) == True:  # [{(2, 'Urban', ('Urban2', 35))}]
                         result1 = []
                         e = key
                         e = list(*e)
-                        e = tuple(*e)
+                        e = tuple(*e)  # (2, 'Urban', ('Urban2', 35))
                         for key in e:
                             if isinstance(key, int) == True:
-                                a = key
+                                a = key         # a = 2
                             if isinstance(key, str) == True:
-                                a = a + len(key)
+                                a = a + len(key)        # a = 2 + Urban
                             if isinstance(key, tuple) == True:
                                 f = list(key)
-                                f = tuple(f)
+                                f = tuple(f)        # ('Urban2', 35)
                                 for key in f:
                                     if isinstance(key, int) == True:
-                                        a = key
+                                        a = key     # 35
                                         result1.append(a)
                                     if isinstance(key, str) == True:
-                                        a = a + len(key)
+                                        a = a + len(key)    # 13 = (2 + Urban + Urban2)
                                         result1.append(a)
-                        a4 = sum(result1)
+                        a5 = sum(result1)  # a5 = 48
                         break
-    return  a
 
+
+    return z
 
 data_structure = [
     [1, 2, 3],
